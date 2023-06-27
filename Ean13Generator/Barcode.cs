a843f4b7479f993353c6b44515a67a09ac08bbeb
Saeed Ahmadi <saeedahmadi694@gmail.com>
2023-06-27 17:00:44 +0430

    done

--- Ean13Generator/Barcode.cs
+++ Ean13Generator/Barcode.cs
@@ -55,17 +55,14 @@ namespace Ean13Generator
     {
         private readonly string _data;
         private readonly string _caption;
+        private readonly int _lineWidth = 1;
         private readonly int _captionSpace = 20;
         private string _encodedData;
         private readonly Color _foregroundColor = Color.Black;
         private readonly Color _backgroundColor = Color.White;
         private int _width = 600;
         private int _height = 250;
-        private readonly bool _showLabel = false;
         private readonly List<int> _specialLine = new() { 0, 2, 46, 48, 92, 94 };
-
-        private Font _labelFont;
-
         private readonly AlignmentPosition _alignmentPosition = AlignmentPosition.Center;
 
         public Barcode(string data, string caption)
@@ -75,104 +72,11 @@ namespace Ean13Generator
 
             InitializeType();
         }
-        public Barcode(string data, bool showLabel, string caption)
+        public Barcode(string data, int lineWidth, int height, string caption)
         {
+            _lineWidth = lineWidth;
             _data = data;
-            _showLabel = showLabel;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, bool showLabel, Font labelFont, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _labelFont = labelFont;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, int width, int height, string caption)
-        {
-            _data = data;
-            _width = width;
             _height = height;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, bool showLabel, int width, int height, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _width = width;
-            _height = height;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, bool showLabel, int width, int height, Font labelFont, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _width = width;
-            _height = height;
-            _labelFont = labelFont;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, bool showLabel, int width, int height,
-            AlignmentPosition alignmentPosition, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _width = width;
-            _height = height;
-            _alignmentPosition = alignmentPosition;
-            _caption = caption;
-
-            InitializeType();
-        }
-
-        public Barcode(string data, bool showLabel, int width, int height,
-            AlignmentPosition alignmentPosition, Font labelFont, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _width = width;
-            _height = height;
-            _alignmentPosition = alignmentPosition;
-            _labelFont = labelFont;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, bool showLabel, int width, int height,
-            AlignmentPosition alignmentPosition, Color backgroundColor, Color foregroundColor, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _width = width;
-            _height = height;
-            _alignmentPosition = alignmentPosition;
-            _backgroundColor = backgroundColor;
-            _foregroundColor = foregroundColor;
-            _caption = caption;
-
-            InitializeType();
-        }
-        public Barcode(string data, bool showLabel, int width, int height,
-            AlignmentPosition alignmentPosition, Color backgroundColor, Color foregroundColor, Font labelFont, string caption)
-        {
-            _data = data;
-            _showLabel = showLabel;
-            _width = width;
-            _height = height;
-            _alignmentPosition = alignmentPosition;
-            _backgroundColor = backgroundColor;
-            _foregroundColor = foregroundColor;
-            _labelFont = labelFont;
             _caption = caption;
 
             InitializeType();
@@ -283,19 +187,18 @@ namespace Ean13Generator
             float labelHeight = 0F, labelWidth = 0F;
             TextOptions labelTextOptions = null;
 
-            if (_showLabel)
+
+            labelTextOptions = new TextOptions(GetEffeciveFont())
             {
-                labelTextOptions = new TextOptions(GetEffeciveFont())
-                {
-                    Dpi = 200,
-                };
+                Dpi = 200,
+            };
 
-                var labelSize = TextMeasurer.Measure(_data, labelTextOptions);
-                labelHeight = labelSize.Height;
-                labelWidth = labelSize.Width;
-            }
+            var labelSize = TextMeasurer.Measure(_data, labelTextOptions);
+            labelHeight = labelSize.Height;
+            labelWidth = labelSize.Width;
 
-            var iBarWidth = _width / _encodedData.Length;
+            int _width = _encodedData.Count() * _lineWidth + 50;
+
             var shiftAdjustment = 0;
             var iBarWidthModifier = 1;
 
@@ -305,7 +208,7 @@ namespace Ean13Generator
                     shiftAdjustment = (_width % _encodedData.Length) / 2;
                     break;
                 case AlignmentPosition.Left:
-                    shiftAdjustment = 0;
+                    shiftAdjustment = 12;
                     break;
                 case AlignmentPosition.Right:
                     shiftAdjustment = (_width % _encodedData.Length);
@@ -315,13 +218,14 @@ namespace Ean13Generator
                     break;
             }
 
-            if (iBarWidth <= 0)
+            if (_lineWidth <= 0)
                 throw new Exception(
                     "EGENERATE_IMAGE-2: Image size specified not large enough to draw image. (Bar size determined to be less than 1 pixel)");
 
             //draw image
             var pos = 0;
-            var halfBarWidth = (int)(iBarWidth * 0.5);
+            var halfBarWidth = (int)(_lineWidth * 0.5);
+
 
             var image = new Image<Rgba32>(_width, _height + _captionSpace);
 
@@ -329,18 +233,17 @@ namespace Ean13Generator
             {
                 var size = TextMeasurer.Measure(_caption, labelTextOptions);
                 int labelY = 0;
-                int labelX = 0;
+                int labelX = 5;
 
-                var font = GetEffeciveFont();
-                image.Mutate(x => x.DrawText(_caption, font, _foregroundColor, new Point(labelX, labelY)));
+                image.Mutate(x => x.DrawText(_caption, GetEffeciveFont(), _foregroundColor, new Point(labelX, labelY)));
             }
-
 
             image.Mutate(imageContext =>
             {
                 imageContext.BackgroundColor(_backgroundColor);
 
-                var pen = new Pen(_foregroundColor, iBarWidth / iBarWidthModifier);
+
+                var pen = new Pen(_foregroundColor, _lineWidth);
                 var drawingOptions = new DrawingOptions
                 {
                     GraphicsOptions = new GraphicsOptions
@@ -351,6 +254,7 @@ namespace Ean13Generator
                 };
 
                 var moreLength = 0;
+
                 while (pos < _encodedData.Length)
                 {
 
@@ -360,35 +264,30 @@ namespace Ean13Generator
                         {
                             moreLength = 8;
                         }
+                        float x = shiftAdjustment + halfBarWidth + (pos * _lineWidth);
                         imageContext.DrawLines(drawingOptions, pen,
-                            new PointF(pos * iBarWidth + shiftAdjustment + halfBarWidth, _captionSpace),
-                            new PointF(pos * iBarWidth + shiftAdjustment + halfBarWidth, _height + moreLength)
+                            new PointF(x + (_lineWidth / 2), _captionSpace),
+                            new PointF(x + (_lineWidth / 2), _height + moreLength)
                         );
                         moreLength = 0;
                     }
-
                     pos++;
                 }
             });
 
+            var font = GetEffeciveFont();
+            float xEnd = shiftAdjustment + (_lineWidth / 2) - 10;
 
-            if (_showLabel)
-            {
-                var font = GetEffeciveFont();
-                float xEnd = iBarWidth + shiftAdjustment + halfBarWidth - 10;
+            float yPosition = _height + 2;
 
-                float yPosition = _height + 2;
+            image.Mutate(c => c.DrawText(_data.Substring(0, 1), font, Color.Black, new PointF(xEnd, yPosition)));
 
-                image.Mutate(c => c.DrawText(_data.Substring(0, 1), font, Color.Black, new PointF(xEnd, yPosition)));
+            xEnd += 20 * _lineWidth;
+            image.Mutate(c => c.DrawText(_data.Substring(1, 6), font, Color.Black, new PointF(xEnd, yPosition)));
 
-                xEnd += 20 * iBarWidth;
-                image.Mutate(c => c.DrawText(_data.Substring(1, 6), font, Color.Black, new PointF(xEnd, yPosition)));
+            xEnd += 40 * _lineWidth;
+            image.Mutate(c => c.DrawText(_data.Substring(7), font, Color.Black, new PointF(xEnd, yPosition)));
 
-                xEnd += 46 * iBarWidth;
-                image.Mutate(c => c.DrawText(_data.Substring(7), font, Color.Black, new PointF(xEnd, yPosition)));
-
-
-            }
 
             return image;
         }
@@ -396,8 +295,8 @@ namespace Ean13Generator
         private Font GetEffeciveFont()
         {
             FontCollection collection = new();
-            //FontFamily family = collection.Add("arial.ttf");
-            FontFamily family = collection.Add("wwwroot/assets/fonts/arial.ttf");
+            FontFamily family = collection.Add("arial.ttf");
+            //FontFamily family = collection.Add("wwwroot/assets/fonts/arial.ttf");
 
             return family.CreateFont(10, FontStyle.Regular);
         }
--- Ean13Generator/Program.cs
+++ Ean13Generator/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Ean13Generator;
-using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 
-var barcode = new Barcode("9786005691757", true,130,50, $"ISBN : 978-600-5691-75-7");
+Console.WriteLine("dasdas");
+var barcode = new Barcode("9786005691757", 2, 90, $"ISBN : 978-600-5691-75-7");
 var img = barcode.GetImage();
 
 img.Save("b.jpg");
